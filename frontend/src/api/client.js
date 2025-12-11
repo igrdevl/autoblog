e687654c28617bec39476e155aefcc9e7fddec4e
Igor Souza <igrdev@hotmail.com>
2025-12-11 10:22:56 -0300

    Changing backend IP and API Base URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,9 +1,11 @@
+const API_BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL || "http://localhost:3000";
+
 export async function getArticles() {
-  const res = await fetch("http://localhost:3000/articles");
+  const res = await fetch(`${API_BASE_URL}/articles`);
   return res.json();
 }
 
 export async function getArticleById(id) {
-  const res = await fetch(`http://localhost:3000/articles/${id}`);
+  const res = await fetch(`${API_BASE_URL}/articles/${id}`);
   return res.json();
 }
